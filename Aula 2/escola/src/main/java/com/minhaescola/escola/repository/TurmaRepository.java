0a7dc5bde90f11d7792c6b13e04d0b1b9a3d1080
Lais <laisbasso@live.com>
2020-07-29 10:56:29 -0300

    Atualizando

--- Aula 2/escola/src/main/java/com/minhaescola/escola/repository/TurmaRepository.java
+++ Aula 2/escola/src/main/java/com/minhaescola/escola/repository/TurmaRepository.java
@@ -9,9 +9,4 @@ import com.minhaescola.escola.model.Turma;
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
 	
-	/* isso fica aqui?
-	 
-	 public List<Turma> findAllTurma();
-	
-	public List<Turma> findByIDTurma(Long id);*/
 }
